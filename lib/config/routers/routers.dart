c04efe9cc0d43b46d0c104f4bb16a0f4ffd949c2
July Suk <humbertoxd25@gmail.com>
2024-04-24 23:57:45 -0700

    commit inicial

---
+++ lib/config/routers/routers.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_push_notification/presentation/screens/HomeScreen.dart';
+import 'package:go_router/go_router.dart';
+
+final appRouter = GoRouter(routes: [
+  GoRoute(
+    path: '/',
+    builder: (context, state) => const HomeScreen(),
+  )
+]);
