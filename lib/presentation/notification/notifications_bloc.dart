0ca51537693863118fb0dc8d71250214f5b4ddd8
July Suk <humbertoxd25@gmail.com>
2024-05-08 22:00:00 -0700

    onForegroundMessage

--- lib/presentation/notification/notifications_bloc.dart
+++ lib/presentation/notification/notifications_bloc.dart
@@ -13,6 +13,7 @@ class NotificationsBloc extends Bloc<NotificationsEvent, NotificationsState> {
   NotificationsBloc() : super(const NotificationsState()) {
     on<NotificationStatusChanged>(_notificationsStatusChanged);
     _checkPermissionFcM();
+    _onForegroundMessage();
   }
 
   static Future<void> initializeFCM() async {
@@ -25,6 +26,19 @@ class NotificationsBloc extends Bloc<NotificationsEvent, NotificationsState> {
       NotificationStatusChanged event, Emitter<NotificationsState> emit) {
     emit(state.copywith(status: event.status));
     _getFcmToken();
+  }
+
+  void _handleRemorteMessage(RemoteMessage message) {
+    print('Got a message whilst in the foreground!');
+    print('Message data: ${message.data}');
+
+    if (message.notification != null) {
+      print('Message also contained a notification: ${message.notification}');
+    }
+  }
+
+  void _onForegroundMessage() {
+    FirebaseMessaging.onMessage.listen(_handleRemorteMessage);
   }
 
   void _checkPermissionFcM() async {
